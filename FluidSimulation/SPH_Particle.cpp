ceafd0e6bb6b0d571fa88b9d15e83425000b18d6
lumberman88 <spotifyuser998@gmail.com>
2022-12-23 00:33:50 +0800

    Encapsulate particle data in struct for easier management.

--- FluidSimulation/SPH_Particle.cpp
+++ FluidSimulation/SPH_Particle.cpp
@@ -10,15 +10,13 @@ SPH_Particle::~SPH_Particle()
 
 void SPH_Particle::init(float m, float d, glm::vec2 pos)
 {
-	this->m = m;
-	this->d = d;
-	this->pos = pos;
-	p = 0.f;
+	curr.init(m, d, pos);
+	prev = curr;
 }
 
 void SPH_Particle::draw(glm::mat4& mvMat, int mvpHandle, Mesh* particleMesh)
 {
-	glm::mat4 mvpMat = mvMat * glm::translate(glm::mat4(1.f), glm::vec3(pos, 0.f));
+	glm::mat4 mvpMat = mvMat * glm::translate(glm::mat4(1.f), glm::vec3(curr.pos, 0.f));
 	glUniformMatrix4fv(mvpHandle, 1, GL_FALSE, glm::value_ptr(mvpMat));
 	glBindVertexArray(particleMesh->getVAO());
 	glDrawElements(GL_TRIANGLES, particleMesh->getTotalIndices(), GL_UNSIGNED_INT, 0);
@@ -26,5 +24,5 @@ void SPH_Particle::draw(glm::mat4& mvMat, int mvpHandle, Mesh* particleMesh)
 
 const glm::vec2& SPH_Particle::getPos()
 {
-	return pos;
+	return curr.pos;
 }
--- FluidSimulation/SPH_Particle.h
+++ FluidSimulation/SPH_Particle.h
@@ -1,13 +1,32 @@
 #pragma once
 #include "MeshBuilder.h"
 
+struct SPH_ParticleData
+{
+	float m, d;	// mass, density
+	glm::vec2 pos;	// pos
+	float p;	// pressure
+
+	void init(float m, float d, glm::vec2 pos)
+	{
+		this->m = m;
+		this->d = d;
+		this->pos = pos;
+		p = 0.f;
+	}
+
+	SPH_ParticleData& operator=(const SPH_ParticleData& copy)
+	{
+		this->m = copy.m;
+		this->d = copy.d;
+		this->pos = copy.pos;
+		p = copy.p;
+	}
+};
+
 class SPH_Particle
 {
-	// given
-	float m, d;
-	glm::vec2 pos;
-	// to be derived
-	float p;
+	SPH_ParticleData prev, curr;
 
 public:
 	SPH_Particle();
